eb0212f552b6ff448f63d1e7bf90a8c39823b612
restlin <restlin@deviling>
2021-02-06 14:33:58 +0400

    [FIX] отображение аватара в pdf

--- views/project/pdf.php
+++ views/project/pdf.php
@@ -2,10 +2,25 @@
 
 /* @var $project app\models\Project */
 use app\helpers\UserHelper;
+use yii\helpers\Html;
 ?>
 <h1><?= $project->name ?></h1>
-<div>Сроки проекта: <?= $project->date_start ?> - <?= $project->date_end ?></div>
-<div>Требуемое финансирование: <?= $project->finance ?> рублей</div>
+<div style="overflow: auto">
+    <div style="float: left; width: 120mm;">
+        Тип: <?= $project->type->name ?><br>
+        Сроки проекта: <?= $project->date_start ?> - <?= $project->date_end ?><br>
+        Требуемое финансирование: <?= $project->finance ?> рублей
+    </div>
+    <div style="float: left; width: 60mm;">
+        <?php 
+            $stream = $project->image ? stream_get_contents($project->image) : false;
+            if ($stream) {
+                $image = 'data:image/jpeg;charset=utf-8;base64,' . base64_encode($stream);
+                echo Html::img($image, ['style' => 'width: 50mm;']);
+            }
+        ?>
+    </div>    
+</div>
 <h2>Команда проекта</h2>
 <ol>
 <?php 
